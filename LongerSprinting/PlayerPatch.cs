b65d8f91d20db73bbc4384865f43098018a63f7c
Anthony Stainton <anthonystainton@gmail.com>
2024-04-03 17:48:30 -0700

    Fixed stamina regen rate.

--- LongerSprinting/PlayerPatch.cs
+++ LongerSprinting/PlayerPatch.cs
@@ -1,11 +1,21 @@
 ﻿using System;
 using HarmonyLib;
-
-[HarmonyPatch(typeof(PlayerController), "Start")]
+using UnityEngine;
+using BepInEx;
+using BepInEx.Configuration;
+[HarmonyPatch(typeof(PlayerController))]
 class PlayerPatch {
+    [HarmonyPrefix]
+    [HarmonyPatch("Start")]
     static bool Prefix(PlayerController __instance) {
         __instance.maxStamina = LongerSprint.MaxStamina.Value;
         __instance.staminaRegRate = LongerSprint.StaminaRegenRate.Value;
         return true;
+    }
+    [HarmonyPostfix]
+    [HarmonyPatch("Update")]
+    static void UpdatePostFix(Player ___player, PlayerController __instance) {
+        LongerSprint.logger.LogInfo($"Plugin Update! kys!");
+        if (!___player.data.isSprinting) ___player.data.currentStamina = Math.Clamp((__instance.staminaRegRate * Time.deltaTime) + ___player.data.currentStamina, 0, __instance.maxStamina);
     }
 }
--- LongerSprinting/Plugin.cs
+++ LongerSprinting/Plugin.cs
@@ -3,13 +3,14 @@ using BepInEx.Configuration;
 using UnityEngine;
 using HarmonyLib;
 
-[BepInPlugin("LongerSprinting", "LongerSprinting", "1.0.0")]
+[BepInPlugin("LongerSprinting", "LongerSprinting", "1.0.2")]
 public class LongerSprint : BaseUnityPlugin {
     internal static ConfigEntry<float>? MaxStamina { get; private set; }
     internal static ConfigEntry<float>? StaminaRegenRate { get; private set; }
+    public static BepInEx.Logging.ManualLogSource logger { get; private set; }
     private void Awake() {
         // Plugin startup logic
-
+        logger = Logger;
         MaxStamina = Config.Bind("General", "MaxStamina", 50f,
             "How much stamina you have.");
         StaminaRegenRate = Config.Bind("General", "StaminaRegenRate", 10f,
