f429aec4ba49e2c15d5db147d4fcea3087fd32b3
Anthony Stainton <anthonystainton@gmail.com>
2024-04-03 20:33:36 -0700

    oh god I spammed console oops

--- LongerSprinting/PlayerPatch.cs
+++ LongerSprinting/PlayerPatch.cs
@@ -10,12 +10,14 @@ class PlayerPatch {
     static bool Prefix(PlayerController __instance) {
         __instance.maxStamina = LongerSprint.MaxStamina.Value;
         __instance.staminaRegRate = LongerSprint.StaminaRegenRate.Value;
+        __instance.sprintMultiplier = LongerSprint.SprintMultiplyer.Value;
+        __instance.movementForce = LongerSprint.BaseMovementSpeed.Value;
         return true;
     }
     [HarmonyPostfix]
     [HarmonyPatch("Update")]
-    static void UpdatePostFix(Player ___player, PlayerController __instance) {
-        LongerSprint.logger.LogInfo($"Plugin Update! kys!");
-        if (!___player.data.isSprinting) ___player.data.currentStamina = Math.Clamp((__instance.staminaRegRate * Time.deltaTime) + ___player.data.currentStamina, 0, __instance.maxStamina);
+    static void UpdatePostFix(Player ___player, PlayerController __instance) {;
+        if (!___player.data.isSprinting && __instance.staminaRegRate !=0f) ___player.data.currentStamina = Math.Clamp((__instance.staminaRegRate * Time.deltaTime) + ___player.data.currentStamina, 0, __instance.maxStamina);
+        if (LongerSprint.IsInfinite.Value) ___player.data.currentStamina = __instance.maxStamina;
     }
 }
--- LongerSprinting/Plugin.cs
+++ LongerSprinting/Plugin.cs
@@ -3,20 +3,32 @@ using BepInEx.Configuration;
 using UnityEngine;
 using HarmonyLib;
 
-[BepInPlugin("LongerSprinting", "LongerSprinting", "1.0.2")]
+[BepInPlugin("LongerSprinting", "LongerSprinting", "1.0.3")]
 public class LongerSprint : BaseUnityPlugin {
     internal static ConfigEntry<float>? MaxStamina { get; private set; }
     internal static ConfigEntry<float>? StaminaRegenRate { get; private set; }
+    internal static ConfigEntry<bool>? IsInfinite { get; private set; }
+    internal static ConfigEntry<float>? SprintMultiplyer { get; private set; }
+    internal static ConfigEntry<float>? BaseMovementSpeed { get; private set; }
+
+
     public static BepInEx.Logging.ManualLogSource logger { get; private set; }
     private void Awake() {
         // Plugin startup logic
         logger = Logger;
-        MaxStamina = Config.Bind("General", "MaxStamina", 50f,
-            "How much stamina you have.");
-        StaminaRegenRate = Config.Bind("General", "StaminaRegenRate", 10f,
-                    "How quickly you regenerate stamina.");
-    
-        Logger.LogInfo($"Plugin LongerSprinting is loaded!");
+        MaxStamina = Config.Bind("General", "MaxStamina", 20f,
+            "The maximum amount of stamina you have in game, The default without the mod is 10.");
+        StaminaRegenRate = Config.Bind("General", "StaminaRegenRate", 2f,
+                    "How quickly you regenerate stamina. This is added on the base stamina regen so if you wanted to not regenerate any extra stamina you may set this to 0");
+
+        SprintMultiplyer = Config.Bind("General", "SprintMultiplyer", 2.3f,
+            "How fast you sprint.");
+        BaseMovementSpeed = Config.Bind("General", "BaseMovementSpeed", 17f,
+                    "How Fast you walk.");
+
+        IsInfinite = Config.Bind("General", "IsInfinite", false,
+                    "Are you able to sprint forever?");
+        Logger.LogInfo($"Plugin LongerSprinting is loaded! v1.0.3");
 
         Harmony harmony = new Harmony("sprintLonger");
         harmony.PatchAll();
